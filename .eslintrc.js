4977cb0e2271c046482ff732ae474a4a3e1c1ab1
Charlie Carson <charliecarson@gmail.com>
2021-06-01 18:12:04 -0700

    updated the eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  extends: [
-    'standard'
+  'extends': [
+    'google',
   ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
   },
-  rules: {
-  }
-}
+  'rules': {
+    "max-len": ["error", {"code": 120}],
+    "require-jsdoc": "off",
+    "object-curly-spacing": ["error", "always"],
+    "indent": ["error", 2, {
+      "CallExpression": {"arguments": 1},
+      "SwitchCase": 1,
+    }],
+    "arrow-parens": ["error", "as-needed"],
+  },
+};
