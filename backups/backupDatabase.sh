83c859752ae28082de57c4ff926ccc69faf26015
Charlie Carson <charliecarson@gmail.com>
2022-02-08 19:49:21 -0500

    backup to google drive folder

--- backups/backupDatabase.sh
+++ backups/backupDatabase.sh
@@ -1,27 +1,35 @@
-dateStr=`date "+%Y-%m-%d"`
+host=`hostname`
+host=${host%.local}
+
+dateStr=`date "+%Y-%m-%d-00:00:00"`
+
+while getopts f flag
+do
+    case "${flag}" in
+        f) dateStr=`date "+%Y-%m-%d-%H:%M:%S"`
+    esac
+done
 
 function compressFile {
   filename=$1
   compressedFilename=$2
 
-  if [ -e $filename ] && [ ! -e $compressedFilename ]; then
+  if [ -e "$filename" ] && [ ! -e "$compressedFilename" ]; then
     echo "compressing $filename"
-    /opt/homebrew/bin/7z a -sdel $filename.7z $filename
+    /opt/homebrew/bin/7z a -sdel "$filename.7z" "$filename"
   fi
 }
 
 function dumpDatabase {
-  dumpFilename="$HOME/backups/$dateStr.dump"
+  dumpFilename="$HOME/My Drive/backups/$dateStr.$host.dump"
   compressedFilename="$dumpFilename.7z"
 
-  if [ ! -e $dumpFilename ] && [ ! -e $compressedFilename ]; then
+  if [ ! -e "$dumpFilename" ] && [ ! -e "$compressedFilename" ]; then
     echo "dumping database to $dumpFilename";
-    sqlite3 $HOME/github/rfb2-server/database.sqlite .dump > $dumpFilename
+    sqlite3 "$HOME/github/rfb2-server/database.sqlite" .dump > "$dumpFilename"
   fi
 
-  compressFile $dumpFilename $compressedFilename
+  compressFile "$dumpFilename" "$compressedFilename"
 }
 
 dumpDatabase
-
-python3 ~/github/rfb2-server/backups/uploadBackup.py
