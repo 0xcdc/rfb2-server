a4b5adca617c1349011860c11b6f688f236dadae
Charlie Carson <charliecarson@gmail.com>
2022-02-10 20:19:37 -0500

    add launch agent for the web site

--- launchd/remove_launchd.sh
+++ launchd/remove_launchd.sh
@@ -1,2 +1,4 @@
 sudo launchctl unload /Library/LaunchDaemons/com.RFB.backupDatabase.plist
+sudo launchctl unload /Library/LaunchDaemons/com.RFB.launchServer.plist
 sudo rm /Library/LaunchDaemons/com.RFB.backupDatabase.plist
+sudo rm /Library/LaunchDaemons/com.RFB.launchServer.plist
