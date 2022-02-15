165b77bc23b68e572756f5bebfdd72b1fe729128
Charlie Carson <charliecarson@gmail.com>
2022-02-14 20:17:02 -0500

    backup and remove existing database

--- backups/restoreDatabase.sh
+++ backups/restoreDatabase.sh
@@ -10,21 +10,23 @@ if [ $# != 1 ]; then
 fi
 
 source=$1
+compressedFilename=`ls ~/My\ Drive/backups | sort | grep "\.$source\." | tail -1`
 
-if [ -e "$HOME/github/rfb2-server/database.sqlite" ]; then
-  echo "database exists already"
-  exit -2
-fi
-
-compressedFilename=`ls ~/My\ Drive/backups | sort | grep ".$source." | tail -1`
-
-if [ $compressedFilename == "" ]; then
+if [ "$compressedFilename" == "" ]; then
   echo "could not find backup for $source"
   exit -3
 fi
+
 compressedFilename="$HOME/My Drive/backups/$compressedFilename"
+
+sudo launchctl stop com.RFB.launchServer
+
+if [ -e "$HOME/github/rfb2-server/database.sqlite" ]; then
+  echo "backing up existing database"
+  "$HOME/github/rfb2-server/backups/backupDatabase.sh" -f
+  rm "$HOME/github/rfb2-server/database.sqlite"
+fi
 
 echo "restoring from $compressedFilename"
 /opt/homebrew/bin/7z x -so "$compressedFilename" | sqlite3 $HOME/github/rfb2-server/database.sqlite
-sudo launchctl stop com.RFB.launchServer
 sudo launchctl start  com.RFB.launchServer
