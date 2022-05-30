2fddfdfaff224230a8bd8e2c4af9c7804c07c8e3
Charlie Carson <charliecarson@gmail.com>
2022-05-29 19:40:20 -0500

    added test cases for visitsForMonth & visitsForHousehold

--- test/visit.test.js
+++ test/visit.test.js
@@ -28,5 +28,51 @@ describe('visit', () => {
     })
   })
 
+  it('visits for household', (done) => {
+    request.post('/graphql')
+    .send({ query: `{
+      visitsForHousehold(householdId: 100) {
+        id householdId householdVersion date
+      }
+    }` })
+    .expect(200)
+    .end((err, res) => {
+      // res will contain array of all clients
+      if (err) return done(err);
+      should.exist(res.body.data);
+      should.exist(res.body.data.visitsForHousehold);
+      // assume there are clients in the database
+      res.body.data.visitsForHousehold.should.have.lengthOf.above(0);
+      res.body.data.visitsForHousehold[0].should.have.property('householdId');
+      res.body.data.visitsForHousehold[0].should.have.property('householdVersion');
+      res.body.data.visitsForHousehold[0].should.have.property('date');
+      res.body.data.visitsForHousehold[0].should.have.property('id');
+      done();
+    })
+  })
+
+  it('visits for month', (done) => {
+    request.post('/graphql')
+    .send({ query: `{
+      visitsForMonth(year: 2021, month: 12) {
+        id householdId householdVersion date
+      }
+    }` })
+    .expect(200)
+    .end((err, res) => {
+      // res will contain array of all clients
+      if (err) return done(err);
+      should.exist(res.body.data);
+      should.exist(res.body.data.visitsForMonth);
+      // assume there are clients in the database
+      res.body.data.visitsForMonth.should.have.lengthOf.above(0);
+      res.body.data.visitsForMonth[0].should.have.property('householdId');
+      res.body.data.visitsForMonth[0].should.have.property('householdVersion');
+      res.body.data.visitsForMonth[0].should.have.property('date');
+      res.body.data.visitsForMonth[0].should.have.property('id');
+      done();
+    })
+  })
+
 
 });
