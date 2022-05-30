3c1bca846b5eb08125d11e74164785ac7e6f18ee
Charlie Carson <charliecarson@gmail.com>
2022-05-29 19:16:53 -0500

    add unit test for first visit of year

---
+++ test/visit.test.js
@@ -0,0 +1,32 @@
+import supertest from 'supertest';
+import chai from 'chai';
+const should = chai.should();
+const url = `http://localhost:4000`;
+const request = supertest(url);
+
+describe('GraphQL', () => {
+  it('first visits for year', (done) => {
+    request.post('/graphql')
+    .send({ query: `{
+      firstVisitsForYear(year: 2022) {
+        id householdId householdVersion date
+      }
+    }` })
+    .expect(200)
+    .end((err, res) => {
+      // res will contain array of all clients
+      if (err) return done(err);
+      should.exist(res.body.data);
+      should.exist(res.body.data.firstVisitsForYear);
+      // assume there are clients in the database
+      res.body.data.firstVisitsForYear.should.have.lengthOf.above(0);
+      res.body.data.firstVisitsForYear[0].should.have.property('householdId');
+      res.body.data.firstVisitsForYear[0].should.have.property('householdVersion');
+      res.body.data.firstVisitsForYear[0].should.have.property('date');
+      res.body.data.firstVisitsForYear[0].should.have.property('id');
+      done();
+    })
+  })
+
+
+});
