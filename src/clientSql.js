831ca041bdbc22dcf21df887317757c331e65f6f
Charlie Carson <charliecarson@gmail.com>
2022-06-26 20:22:13 -0400

    moved incrementHousehold into householdSql.js

--- src/clientSql.js
+++ src/clientSql.js
@@ -1,5 +1,5 @@
 import database from './database';
-import { incrementHouseholdVersion } from './increment';
+import { incrementHouseholdVersion } from './householdSql';
 
 function addHouseholdInfo(clientList) {
   clientList.forEach(client => {
--- src/householdSql.js
+++ src/householdSql.js
@@ -1,7 +1,36 @@
 import { loadAllCities, loadCityById } from './citySql';
 import { loadAllClients, loadClientsForHouseholdId } from './clientSql';
 import database from './database';
-import { incrementHouseholdVersion } from './increment';
+
+export function incrementHouseholdVersion(conn, householdId) {
+  return conn.execute(
+    `
+    insert into household (id, version, address1, address2, cityId, zip, note, incomeLevelId)
+      select id, version+1, address1, address2, cityId, zip, note, incomeLevelId
+      from household
+      where household.id = :householdId
+        and not exists (
+          select 1
+          from household h2
+          where household.id = h2.id
+            and h2.version > household.version
+        )`,
+    { householdId })
+    .then( () => conn.getMaxVersion('household', householdId))
+    .then( householdVersion =>
+      conn.execute(
+
+        `
+        insert into household_client_list (householdId, householdVersion, clientId, clientVersion)
+          select householdId, :householdVersion, clientId, clientVersion
+            from household_client_list
+            where householdId = :householdId
+              and householdVersion = :householdVersion - 1`,
+        { householdVersion, householdId })
+        .then( () => householdVersion)
+    );
+}
+
 
 function selectById({ id, version }) {
   version = version ? Promise.resolve(version) : database.getMaxVersion('household', id);
