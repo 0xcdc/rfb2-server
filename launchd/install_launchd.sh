1a170f8eccb1122230d8f047f0b896542ac63e2d
Charlie Carson <charliecarson@gmail.com>
2023-06-26 19:46:26 -0400

    removed the launchd scripts

--- launchd/install_launchd.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /Library/LaunchDaemons
-sudo cp com.RFB.backupDatabase.plist /Library/LaunchDaemons/
-sudo cp com.RFB.launchServer.plist /Library/LaunchDaemons/
-sudo launchctl load /Library/LaunchDaemons/com.RFB.backupDatabase.plist
-sudo launchctl load /Library/LaunchDaemons/com.RFB.launchServer.plist
--- launchd/remove_launchd.sh
+++
@@ -1,4 +0,0 @@
-sudo launchctl unload /Library/LaunchDaemons/com.RFB.backupDatabase.plist
-sudo launchctl unload /Library/LaunchDaemons/com.RFB.launchServer.plist
-sudo rm /Library/LaunchDaemons/com.RFB.backupDatabase.plist
-sudo rm /Library/LaunchDaemons/com.RFB.launchServer.plist
