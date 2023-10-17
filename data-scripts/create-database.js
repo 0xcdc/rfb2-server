fc684935e23ed5c3845eb08f9d60839ae81598b6
Long Pham <92423665+phamphilong1604@users.noreply.github.com>
2023-10-17 07:58:05 -0700

    Move credentials.pl to js file  (#4)
    * added some random address
    
    * update createHouseHold function
    
    * Fixed createHousehold function
    
    * move create-database perl to js
    
    * convert credentials.pl to JS
    
    * credentials pl to js fixed eslint
    
    * edited credentials.pl to js with eslint fixed
    
    ---------
    
    Co-authored-by: ryanlp <ryanlp@Ubuntu22.10.myguest.virtualbox.org>

---
+++ data-scripts/create-database.js
@@ -0,0 +1,92 @@
+import { exec as execAsync } from 'child_process';
+import { existsSync } from 'node:fs';
+import { promises as fs } from 'fs';
+import { promisify } from 'util';
+import readline from 'readline';
+
+
+const exec = promisify(execAsync);
+
+function fileExists(fileName) {
+  return existsSync(fileName);
+}
+
+function askQuestion(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return rl.question(question).finally( () => rl.close());
+}
+
+function execCmd(description, cmd) {
+  console.log(`\n${description}\n`);
+  console.log(cmd);
+  return exec(cmd)
+    .then(({ stdout, stderr }) => {
+      console.log(stdout);
+      console.error(stderr);
+    })
+    .catch( error => {
+      console.error(error);
+    });
+}
+async function main() {
+  const credentialsFile = 'credentials.js';
+
+  if (!(await fileExists(credentialsFile))) {
+    const mysqlUsername = await askQuestion('What username do you want to use to login to the foodbank database? ');
+    const mysqlPassword = await askQuestion('What password do you want to use to login to the foodbank database? ');
+    const websiteUsername = await askQuestion(
+      'What username do you want to use to login to the foodbank website (blank for none)? ');
+    const websitePassword = await askQuestion(
+      'What password do you want to use to login to the foodbank website (blank for none)? ');
+    const googleProjectName = await askQuestion('What google project do you (blank for none)? ');
+    const googleApiKey = await askQuestion('What is the api key to use with the google project (blank for none)? ');
+
+    const credentials = {
+      mysqlUsername,
+      mysqlPassword,
+      mysqlHost: process.env.GAE_ENV ? '' : '127.0.0.1',
+      mysqlDatabase: 'foodbank',
+      websiteUsername,
+      websitePassword,
+      googleProjectName,
+      googleApiKey,
+    };
+
+    await fs.writeFile(
+      credentialsFile,
+      `export const credentials = ${JSON.stringify(credentials, null, 2)};\nexport default credentials;\n`
+    );
+    console.log('Overwriting credentials.js file');
+  }
+
+  const { stdout: credentialsObject } =
+    await exec(`node --input-type="module" -e '
+      import {credentials} from "./credentials.js";
+      console.log(JSON.stringify(credentials));'
+    `);
+
+
+  await execCmd('Creating a new foodbank database', 'sudo mysql < data-scripts/latest_schema.sql');
+  await execCmd('Populating fact tables', 'sudo mysql < data-scripts/fact-tables.sql');
+
+  const createUserSql = `
+    DROP USER IF EXISTS '${credentialsObject.mysqlUsername}'@'localhost';
+    CREATE USER 
+    '${credentialsObject.mysqlUsername}'@'localhost' IDENTIFIED BY '${credentialsObject.mysqlPassword}';
+    GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, REFERENCES, 
+    ALTER ON *.* TO '${credentialsObject.mysqlUsername}'@'localhost' WITH GRANT OPTION;
+  `;
+  await execCmd('Creating db user', `echo "${createUserSql}" | sudo mysql`);
+  console.log('\nDONE!\n');
+}
+
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
+
+
