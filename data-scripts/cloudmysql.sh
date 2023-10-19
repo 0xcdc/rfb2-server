4403c0d916f4d59297cee264562c98447ea2591c
Charlie Carson <charliecarson@gmail.com>
2023-10-18 21:03:57 -0500

    changed Ip to IP

--- data-scripts/cloudmysql.sh
+++ data-scripts/cloudmysql.sh
@@ -1,5 +1,5 @@
 username=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlUsername)'`
 password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlPassword)'`
-hostIP=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlCloudPublicIp)'`
+hostIP=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlCloudPublicIP)'`
 
 mysql -h$hostIP -u$username -p$password foodbank
