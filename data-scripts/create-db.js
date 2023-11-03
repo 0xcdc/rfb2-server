93f979123a76e54fae933f185e3597bcd8b20f4a
Charlie Carson <charliecarson@gmail.com>
2023-11-02 20:19:59 -0500

    eslint fixes

--- data-scripts/create-db.js
+++ data-scripts/create-db.js
@@ -18,10 +18,10 @@ function askQuestion(question) {
   });
 
   return new Promise( (resolve, reject) => {
-    rl.question(question, (answer) => {
+    rl.question(question, answer => {
       resolve(answer);
       rl.close();
-    })
+    });
   });
 }
 
