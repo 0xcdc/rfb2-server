a1205ff6f49b5ab62267d0961d32ba128ec3bea5
Charlie Carson <charliecarson@gmail.com>
2023-12-11 22:08:55 -0500

    add an index to speed up

--- data-scripts/delete-dup-visits.sql
+++ data-scripts/delete-dup-visits.sql
@@ -1,12 +1,31 @@
-create temporary table ids_to_delete as (
-  select id
+create temporary table visit_temp as (
+  select *
   from visit
-  where exists (
-    select *
-    from visit v2
-    where v2.date = visit.date
-      and v2.householdId = visit.householdId
-      and v2.id < visit.id)
 );
 
-delete from visit where id in (select id from ids_to_delete);
+alter table visit_temp
+  add index (date, householdId);
+
+create temporary table duplicate_visits as (
+  select date, householdId, min(id) as m
+  from visit_temp
+  group by date, householdId
+  having count(*) > 1
+);
+
+create temporary table visits_to_delete as (
+  select *
+  from visit v
+  where exists (
+    select *
+    from duplicate_visits dv
+    where dv.date = v.date
+      and dv.householdId = v.householdId
+      and dv.m <> v.id
+    )
+);
+
+select * from visits_to_delete;
+
+delete from visit where id in (select id from visits_to_delete);
+
