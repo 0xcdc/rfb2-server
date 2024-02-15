c2ca0b59651b94c175f3254669d47eadcda22647
Charlie Carson <charliecarson@gmail.com>
2024-02-14 22:36:55 -0500

    temp script to do the json upgrade

---
+++ data-scripts/upgrade.sh
@@ -0,0 +1,4 @@
+cat data-scripts/cleanup_orphaned_rows.sql | npm run mysql
+cat data-scripts/convert-city-locatio-to-json.sql | npm run mysql
+cat data-scripts/flatten_household.sql | npm run mysql
+cat data-scripts/household_visit.sql | npm run mysql
