8d68822e17bebe801fdac28a1635bd1f45de377e
Charlie Carson <charliecarson@gmail.com>
2024-02-15 21:17:59 -0500

    remove test for singular endpoints (endspoints removed)

--- test/lookup.test.js
+++ test/lookup.test.js
@@ -9,32 +9,26 @@ const request = supertest(url);
 describe('lookups', () => {
   const lookups = [{
     plural: 'ethnicities',
-    singular: 'ethnicity',
     count: 3,
   },
   {
     plural: 'genders',
-    singular: 'gender',
     count: 4,
   },
   {
     plural: 'incomeLevels',
-    singular: 'incomeLevel',
     count: 5,
   },
   {
     plural: 'militaryStatuses',
-    singular: 'militaryStatus',
     count: 4,
   },
   {
     plural: 'races',
-    singular: 'race',
     count: 9,
   },
   {
     plural: 'yesNos',
-    singular: 'yesNo',
     count: 3,
   }];
 
@@ -51,35 +45,6 @@ describe('lookups', () => {
           should.exist(res.body.data);
           should.exist(res.body.data[lookup.plural]);
           res.body.data[lookup.plural].should.have.lengthOf(lookup.count);
-          done();
-        });
-    });
-
-    it(`Returns ${lookup.singular} with id = 1`, done => {
-      request.post('/graphql')
-        .auth(credentials.websiteUsername, credentials.websitePassword)
-        .send({ query: `{ ${lookup.singular}(id: 1) { id value } }` })
-        .expect(200)
-        .end((err, res) => {
-        // res will contain array with one user
-          if (err) return done(err);
-          should.exist(res.body.data);
-          should.exist(res.body.data[lookup.singular]);
-          res.body.data[lookup.singular].should.have.property('id');
-          res.body.data[lookup.singular].should.have.property('value');
-          done();
-        });
-    });
-
-    it(`non existant ${lookup.singular}`, done => {
-      request.post('/graphql')
-        .auth(credentials.websiteUsername, credentials.websitePassword)
-        .send({ query: `{ ${lookup.singular}(id: -100) { id value } }` })
-        .expect(200)
-        .end((err, res) => {
-          if ( err ) return done(err);
-          should.exist(res.body.data);
-          should.not.exist(res.body.data[lookup.singular]);
           done();
         });
     });
