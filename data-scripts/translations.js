eebf35f23a4011d6444ccd21ed27981b991bd8d8
Charlie Carson <charliecarson@gmail.com>
2024-05-12 19:35:36 -0500

    eslint

--- data-scripts/translations.js
+++ data-scripts/translations.js
@@ -1,9 +1,9 @@
 export const translations = [
-[ 36, 'prompt', 1, '現有家庭成员：' ] ,
-[ 36, 'prompt', 2, '현재 가구 구성원:' ] ,
-[ 36, 'prompt', 3, 'Текущие члены семьи:' ] ,
-[ 36, 'prompt', 4, 'Xubnaha qoyska ee hadda:' ] ,
-[ 36, 'prompt', 5, 'Miembros actuales del hogar:' ] ,
-[ 36, 'prompt', 6, 'Поточні члени домогосподарства:' ] ,
-[ 36, 'prompt', 7, 'Thành viên hộ gia đình hiện tại:' ] ,
+  [36, 'prompt', 1, '現有家庭成员：'],
+  [36, 'prompt', 2, '현재 가구 구성원:'],
+  [36, 'prompt', 3, 'Текущие члены семьи:'],
+  [36, 'prompt', 4, 'Xubnaha qoyska ee hadda:'],
+  [36, 'prompt', 5, 'Miembros actuales del hogar:'],
+  [36, 'prompt', 6, 'Поточні члени домогосподарства:'],
+  [36, 'prompt', 7, 'Thành viên hộ gia đình hiện tại:'],
 ];
