785954a64421017b9f3ca81ba2c9a96159d21ee8
Charlie Carson <charliecarson@gmail.com>
2026-02-24 11:08:43 -0500

    add certs to helper shell scripts

--- data-scripts/cloudmysql.sh
+++ data-scripts/cloudmysql.sh
@@ -2,4 +2,4 @@ username=`node --input-type=module -e 'import {credentials} from "./credentials.
 password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlPassword)'`
 hostIP=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlCloudPublicIP)'`
 
-mysql -h$hostIP -u$username -p$password foodbank
+mysql -h$hostIP -u$username -p$password foodbank --ssl-mode=VERIFY_CA --ssl-ca=server-ca.pem --ssl-cert=client-cert.pem --ssl-key=client-key.pem
--- data-scripts/dump-production.sh
+++ data-scripts/dump-production.sh
@@ -2,4 +2,4 @@ username=`node --input-type=module -e 'import {credentials} from "./credentials.
 password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlPassword)'`
 hostIP=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlCloudPublicIP)'`
 
-mysqldump -h$hostIP -u$username -p$password --no-tablespaces --set-gtid-purged=OFF --single-transaction foodbank > dump.sql
+mysqldump -h$hostIP -u$username -p$password --no-tablespaces --set-gtid-purged=OFF --single-transaction foodbank --ssl-mode=VERIFY_CA --ssl-ca=server-ca.pem --ssl-cert=client-cert.pem --ssl-key=client-key.pem > dump.sql
