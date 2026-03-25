aee9e9ea8553f2249c94a0b7ee13193b13351f05
Charlie Carson <charliecarson@gmail.com>
2026-03-24 21:33:22 -0400

    command line scripts use the new credentials format

--- data-scripts/cloudmysql.sh
+++ data-scripts/cloudmysql.sh
@@ -1,5 +1,6 @@
-username=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlUsername)'`
-password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlPassword)'`
+export CLOUDSQL=1
+username=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysql.user)'`
+password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysql.password)'`
 hostIP=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlCloudPublicIP)'`
 
 mysql -h$hostIP -u$username -p$password foodbank --ssl-mode=VERIFY_CA --ssl-ca=server-ca.pem --ssl-cert=client-cert.pem --ssl-key=client-key.pem
--- data-scripts/mysql.sh
+++ data-scripts/mysql.sh
@@ -1,3 +1,3 @@
-username=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlUsername)'`
-password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysqlPassword)'`
+username=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysql.user)'`
+password=`node --input-type=module -e 'import {credentials} from "./credentials.js";console.log(credentials.mysql.password)'`
 mysql -u$username -p$password foodbank
